6699b0d4722bb39c141dc12f110a33066547d5bd
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2024-12-09 10:24:54 +0530

    bug: build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
-import Navbar from "@/components/navbar";
+'use client';
+
+import { Button } from "@/components/ui/button";
+import { Title } from "@/components/ui/title";
 
 export default function Home() {
   return (
     <>
-      <Navbar/>
-      <main className="h-full border-x">
-        {/* created job listing Table Here */}
-      </main>
+      <div className="py-3 px-6 flex items-center justify-between border border-t-transparent">
+      <Title> 
+        Job Listing
+      </Title>
       
+      <Button size={"sm"} className="shadow-xl" href="/post-job" >Post a Job</Button>
+    </div>
+
+      <main className="h-full border-x">
+      </main>
     </>
   );
 }
